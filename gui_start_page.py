189d5db8f34bef762bfdcf232188e544ff8e00c7
catawba <32372307+catawba@users.noreply.github.com>
2022-05-16 13:03:45 -0600

    Update gui_start_page.py

--- gui_start_page.py
+++ gui_start_page.py
@@ -1,21 +1,66 @@
 # Write your code here :-)
 import PySimpleGUI as sg
 
+
+def create_cow():
+    pass
+
+def read_cow():
+    pass
+
+def update_cow():
+    pass
+
+def delete_cow():
+    pass
+
+def show_crud():
+    sg.theme('DarkAmber')
+    layout = [ [sg.Button('New Cow'),
+                sg.Button('Show Cow'),
+                sg.Button('Edit Cow'),
+                sg.Button('Delete Cow'),
+                sg.Button('Quit')] ]
+
+    # Create the Window
+    window = sg.Window('Cattle Herd', layout)
+    # Event Loop to process "events" and get the "values" of the inputs
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == 'Quit': # if user closes window or clicks cancel
+            break
+        print('You entered ', event)
+
+    window.close()
+    return event
+
+# main program
+
+# show form to get CRUD
+print(show_crud())
+
+
+
+
+
+
+
+
 sg.theme('DarkAmber')   # Add some color
 # All the stuff inside your window.
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Text('Enter something more '), sg.InputText()],
+layout = [  [sg.Text('Enter the Information for a Cow')],
+            [sg.Text('Cow ID '), sg.InputText()],
+            [sg.Text('Cow Name '), sg.InputText()],
             [sg.Button('Ok'), sg.Button('Cancel')] ]
 
 # Create the Window
-window = sg.Window('Window Title', layout)
+window = sg.Window('Cattle Herd', layout)
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
         break
     print('You entered ', values[0],values[1])
-    print(values)
+
 
 window.close()
